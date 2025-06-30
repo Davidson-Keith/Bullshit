177ea81a76d8af6c4450fd3fc84f4878590ada3c
Keith Davidson <davidson.keith@gmail.com>
2025-06-30 12:24:00 +1000

    Initial commit: Vite React Bullshit Button app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bullshit-container">
+      <button className="bullshit-button">Bullshit</button>
+    </div>
+  );
+}
+
+export default App;
